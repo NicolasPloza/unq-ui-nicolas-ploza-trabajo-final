575aa8b172f821cc120e68bc346c88c53c6d3816
NicolasPloza <nicolasploza@gmail.com>
2025-07-16 17:47:26 -0300

    se soluciono bugs causado por acentos

--- trabajo-final/src/components/KeyBoard.jsx
+++ trabajo-final/src/components/KeyBoard.jsx
@@ -19,12 +19,15 @@ export default function KeyBoard(){
     third: "zxcvbnm".split(""),
   };
 
-  
+  //esto lo hago para ignorar los acentos de la repuesta q me da la api, para no tener q agregar letras con acentos al teclado
+  const clean = (char) => char.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); 
+
+
 
   useEffect(() => {
     
     const styleChar = (char) => {
-      const found = solutions.flat().find(item => item.letter === char);
+      const found = solutions.flat().find(item => clean(item.letter) === clean(char));
       return {
         letter: char,
         style: found?.solution || ""

46b0fa3d3bad17d9e72e9fe0a1806967db207c48
NicolasPloza <nicolasploza@gmail.com>
2025-07-16 21:11:55 -0300

    se agrego interfaz responsive

--- trabajo-final/src/components/KeyBoardRow.jsx
+++ trabajo-final/src/components/KeyBoardRow.jsx
@@ -17,7 +17,7 @@ export default function KeyBoardRow({keys}){
         <div className="key flex row">
             {keys.map( (char, i) =>
                 char.letter === 'Enter' || char.letter === 'Backspace'? (
-                    <button key={i} onClick={handleClick}  className={`flex letter-box enter`}>{char.letter}</button>
+                    <button key={i} onClick={handleClick}  className={`flex enter`}>{char.letter}</button>
                 ): (
                     <button key={i} onClick={handleClick} className={`flex letter-box ${char.style}` }>{char.letter}</button>         
             ))}
